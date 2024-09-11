70afe0245a48e125e653bde041aa21316c67c4db
Gunasundari-0415 <gunabe.ece16@gmail.com>
2024-09-11 09:48:15 +0530

    Add files via upload

---
+++ week1/day2/Chrome.java
@@ -0,0 +1,15 @@
+package week1.day2;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear=2008;
+		char shortcutKey = 'C';
+		System.out.println("Version is "+version+"\nDeveloper is "+developer+"\nBeta type is "+isBeta+"\nRelease Year is"+releaseYear+ "\nShortcut Key is "+shortcutKey);
+	}
+
+}
