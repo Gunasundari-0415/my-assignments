d8ddbf4b9f116a1733c6e06880fcdaf465a7e491
Gunasundari-0415 <gunabe.ece16@gmail.com>
2024-09-20 15:38:37 +0530

    Add files via upload

---
+++ week1/day2/day3/EdgeBrowser.java
@@ -0,0 +1,10 @@
+package week1.day3;
+public class EdgeBrowser {
+
+	public static void main(String[] args) {
+		Browser browserDetails=new Browser();
+		System.out.println(browserDetails.lanuchBrowser("edgebrowser"));
+	browserDetails.loadUrl();
+		
+	}
+}
