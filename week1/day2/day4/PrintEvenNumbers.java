4c2b2f2c51d0c62867cfabb057225f4b4e3db51b
Gunasundari-0415 <gunabe.ece16@gmail.com>
2024-09-20 15:45:59 +0530

    Add files via upload

---
+++ week1/day2/day4/PrintEvenNumbers.java
@@ -0,0 +1,16 @@
+package week1.day4;
+
+public class PrintEvenNumbers {
+
+	public static void main(String[] args) {
+		// even numbers
+		for (int i = 1; i <=20; i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+			
+		}
+
+	}
+
+}
